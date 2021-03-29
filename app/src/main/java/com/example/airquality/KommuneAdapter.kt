815c40fac705b298004e6bebc1219100cccfcf7a
samuelsmb <samuelmb@uio.no>
2021-03-29 14:37:02 +0200

    forside

---
+++ app/src/main/java/com/example/airquality/KommuneAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.airquality
+
+class KommuneAdapter(val kommuner: MutableList<Kommune>): RecyclerView.Adapter<DataAdapter.ViewHolder>() {
+    private val dataList = list
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var name: TextView = view.findViewById(R.id.PLACEHOLDER)
+    }
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.card, viewGroup, false)
+        return ViewHolder(view)
+    }
+    override fun onBindViewHolder(viewHolder: ViewHolder, pos: Int) {
+        viewHolder.name.text = dataList[pos].PLACEHOLDER
+    }
+    override fun getItemCount() = dataList.size
+}
--- app/src/main/java/com/example/airquality/MainActivity.kt
+++ app/src/main/java/com/example/airquality/MainActivity.kt
@@ -30,4 +30,27 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
+    private fun searchCounty(query: String, testList: AirQualityData) {
+        var tempList = mutableListOf<Data>()
+        // creates a list of counties which contain the query
+        for(i in testList.data){
+            if(i.PLACEHOLDER.contains(query, ignoreCase = true)){
+                tempList.add(Data(i.PLACEHOLDER))
+            }
+        }
+        val searchList = AirQualityData(tempList)
+        // creates the recyclerview
+        runOnUiThread {
+            var adapter = DataAdapter(searchList.data)
+            val recyclerView: RecyclerView = findViewById(R.id.recycle)
+            recyclerView.adapter = adapter
+            recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+            var viewHolder = adapter.createViewHolder(recyclerView, searchList.data.size)
+            // Fills the recyclerview
+            for(i in searchList.data){
+                adapter.bindViewHolder(viewHolder, searchList.data.indexOf(i))
+            }
+        }
+    }
 }
