4984a0b698582ac5973a881b1de81eb9334240c0
Birk Hogstad <birk1999@hotmail.no>
2021-04-12 22:17:56 +0200

    spinner

--- app/src/main/java/com/example/airquality/MainActivity.kt
+++ app/src/main/java/com/example/airquality/MainActivity.kt
@@ -19,6 +19,26 @@ data class Areas(val zone: String?, val municipality: String?, val area: String?
 data class Aqis(val index: Number?, val fromValue: Number?, val toValue: Number?, val color: String?, val text: String?, val shortDescription: String?, val description: String?, val advice: String?)
 data class Description(val component: String?, val unit: String?, val aqis: List<Aqis>?)
 
+data class StasjonerKommune(val id: Number?,
+                    val zone: String?,
+                    val municipality: String?,
+                    val area: String?,
+                    val station: String?,
+                    val type: String?,
+                    val eoi: String?,
+                    val latitude: Number?,
+                    val longitude: Number?,
+                    val owner: String?,
+                    val status: String?,
+                    val description: String?,
+                    val firstMeasurment: String?,
+                    val lastMeasurment: String?,
+                    val components: String?,
+                    val isVisible: Boolean?
+)
+
+
+
 data class Stasjon(
     val id: Number?,
     val zone: String?,
@@ -37,7 +57,8 @@ data class Stasjon(
     val timestep: Number?,
     val index: Number?,
     val color: String?,
-    val isValid: Boolean?)
+    val isValid: Boolean?
+)
 
 //Main
 data class Adapter(val kommuneNavn: String?, val fargekode: String?)
@@ -47,8 +68,13 @@ data class Statistics(val text: String?, val shortDescription: String?, val desc
 lateinit var stasjonArray: Array<Stasjon>
 lateinit var areasArray: Array<Areas>
 
+
+
 val adapterList = mutableListOf<Adapter>()
 lateinit var valgtKommune: Adapter
+lateinit var stasjonerValgtKommune: Array<StasjonerKommune>
+
+
 
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/airquality/StatisticsActivity.kt
+++ app/src/main/java/com/example/airquality/StatisticsActivity.kt
@@ -3,31 +3,53 @@ package com.example.airquality
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.coroutines.awaitString
 import com.google.gson.Gson
 import kotlinx.coroutines.runBlocking
 import java.lang.Exception
+import java.util.*
 
-class StatisticsActivity : AppCompatActivity() {
+class StatisticsActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
 
+        lateinit var spinnerlanguages:Spinner
+
         lateinit var statisticsArray: Array<Statistics>
         lateinit var descriptionArray: Array<Description>
+        lateinit var stasjonsNavn : MutableList<String>
         val lookupAqis = "https://api.nilu.no/lookup/aqis?component=pm10"
+        val lookupStations = "https://api.nilu.no/lookup/stations?area="
         val description: TextView = findViewById(R.id.description)
         val by: TextView = findViewById(R.id.by)
+
+
+
+        spinnerlanguages = this.findViewById(R.id.spinner)
+        spinnerlanguages.onItemSelectedListener = this
+
+        //val spinner: Spinner = findViewById(R.id.spinner)
+        //spinner.onItemSelectedListener = this
+
+
+
 
         runBlocking {
             try {
                 descriptionArray = Gson().fromJson(Fuel.get(lookupAqis).awaitString(), Array<Description>::class.java)
                 Log.d("TAG", "onCreate: $descriptionArray")
                 valgtKommune.kommuneNavn?.let { Log.d("valgt Kommune", it) }
-
                 for (x in descriptionArray) {
                     for (y in x.aqis!!) {
                         if (y.color.toString() == valgtKommune.fargekode) {
@@ -37,10 +59,35 @@ class StatisticsActivity : AppCompatActivity() {
                         }
                     }
                 }
+                stasjonerValgtKommune = Gson().fromJson(Fuel.get(lookupStations+ valgtKommune.kommuneNavn?.toLowerCase()).awaitString(), Array<StasjonerKommune>::class.java)
+                valgtKommune.kommuneNavn?.toLowerCase()?.let { Log.d("hei   ", it) }
+                var size = stasjonerValgtKommune.size
+                stasjonsNavn = mutableListOf()
+                for (i in 0..stasjonerValgtKommune.size) {
+                    stasjonsNavn.add(stasjonerValgtKommune[i].station.toString())
+                }
+
+
+                Log.d("station array: ", stasjonerValgtKommune.size.toString())
+
 
             } catch (e : Exception) {
                 Log.d("Error ", e.toString())
             }
         }
+
+        var aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, stasjonsNavn)
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerlanguages.adapter = aa
+
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val by: TextView = findViewById(R.id.by)
+        by.text = "Selected: "+ stasjonerValgtKommune[position]
     }
 }
