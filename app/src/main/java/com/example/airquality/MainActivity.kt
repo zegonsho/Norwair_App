c4e613e9f54a7940a096b4305fe03c7f0fc734b8
sirisoll <sirisoll@uio.no>
2021-04-13 21:45:54 +0200

    navbar xml

--- app/src/main/java/com/example/airquality/MainActivity.kt
+++ app/src/main/java/com/example/airquality/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.airquality
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.coroutines.awaitString
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.gson.Gson
 import kotlinx.coroutines.runBlocking
+
 
 //Main:
 
@@ -16,24 +19,24 @@ import kotlinx.coroutines.runBlocking
 data class Areas(val zone: String?, val municipality: String?, val area: String?, val description: String?)
 
 data class Stasjon(
-    val id: Number?,
-    val zone: String?,
-    val municipality: String?,
-    val area: String?,
-    val station: String?,
-    val eoi: String?,
-    val type: String?,
-    val component: String?,
-    val fromTime: String?,
-    val toTime: String?,
-    val value: Number?,
-    val unit: String?,
-    val latitude: Number?,
-    val longitude: Number?,
-    val timestep: Number?,
-    val index: Number?,
-    val color: String?,
-    val isValid: Boolean?)
+        val id: Number?,
+        val zone: String?,
+        val municipality: String?,
+        val area: String?,
+        val station: String?,
+        val eoi: String?,
+        val type: String?,
+        val component: String?,
+        val fromTime: String?,
+        val toTime: String?,
+        val value: Number?,
+        val unit: String?,
+        val latitude: Number?,
+        val longitude: Number?,
+        val timestep: Number?,
+        val index: Number?,
+        val color: String?,
+        val isValid: Boolean?)
 
 //Main
 data class Adapter(val kommuneNavn: String?, val fargekode: String?)
@@ -47,6 +50,7 @@ class MainActivity : AppCompatActivity() {
 
     private val gson = Gson()
     lateinit var recycle: RecyclerView
+
     //NILU:
     val niluStasjonsdataPM10 = "https://api.nilu.no/aq/utd?&components=pm10"
     val niluLookupAreas = "https://api.nilu.no/lookup/areas"
@@ -54,6 +58,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var bottomNavigation : BottomNavigationView = findViewById(R.id.bottom_navigation) //might have to be initalized before
+        val navigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            //TODO: implement fragement pages
+            when (item.itemId) {
+                R.id.navigation_search -> {
+                    //openFragment(HomeFragment.newInstance("", ""))
+                    //return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_fav -> {
+                    //openFragment(SmsFragment.newInstance("", ""))
+                    //return@OnNavigationItemSelectedListener true
+
+                }
+                R.id.navigation_info -> {
+                    //openFragment(NotificationFragment.newInstance("", ""))
+                    //return@OnNavigationItemSelectedListener true
+
+                }
+            }
+            false
+        }
 
         runBlocking {
             Log.d("first: ", "PASSED")
